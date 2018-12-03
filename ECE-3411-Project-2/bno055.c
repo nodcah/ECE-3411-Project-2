f410920db9d39496115f02149de5816a2b936cc9
Indrid Xhuti <indrid35@gmail.com>
2018-12-03 15:59:09 -0500

    bno updates

--- ECE-3411-Project-2/bno055.c
+++ ECE-3411-Project-2/bno055.c
@@ -4,3 +4,18 @@
  * Created: 12/3/2018 3:50:59 PM
  *  Author: Indrid
  */ 
+
+
+#include "bno055.h"
+
+Vector getBNO055() {
+	
+}
+
+void changeMode() {
+	
+}
+
+void initBNO055() {
+	
+}
--- ECE-3411-Project-2/bno055.h
+++ ECE-3411-Project-2/bno055.h
@@ -9,8 +9,23 @@
 #ifndef BNO055_H_
 #define BNO055_H_
 
+#include <stdint.h>
 
 
+typedef struct {
+	int16_t x;
+	int16_t y;
+	int16_t z;
+} BNO_DATA;
 
+Vector getBNO055() {
+	
+}
+
+void changeMode() {
+	
+}
+
+void initBNO055()
 
 #endif /* BNO055_H_ */
