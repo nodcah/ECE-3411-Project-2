1fbe27e31acec5ccc97b6e9f26da44631f7f1f88
Noah Del Coro <nodcah@gmail.com>
2018-12-03 14:55:51 -0500

    Init directory structure

---
+++ ECE-3411-Project-2/main.c
@@ -0,0 +1,18 @@
+/*
+ * ECE-3411-Project-2.c
+ *
+ * Created: 12/3/2018 2:50:21 PM
+ * Author : nodcah
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
