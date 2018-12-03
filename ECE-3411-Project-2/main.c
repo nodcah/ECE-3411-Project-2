fb862b311803b45dee2b0124317ab3e90b6ac570
Noah Del Coro <nodcah@gmail.com>
2018-12-03 15:07:09 -0500

    Adds TWI and UART files

--- ECE-3411-Project-2/main.c
+++ ECE-3411-Project-2/main.c
@@ -3,16 +3,14 @@
  *
  * Created: 12/3/2018 2:50:21 PM
  * Author : nodcah
- */ 
+ */
 
 #include <avr/io.h>
 
 
-int main(void)
-{
+int main(void) {
     /* Replace with your application code */
-    while (1) 
-    {
+    while (1) {
     }
 }
 
