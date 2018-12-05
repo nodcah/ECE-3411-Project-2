8aed33e367ca7cfb2fa3db8b349f2baf8128c309
Noah Del Coro <nodcah@gmail.com>
2018-12-05 15:50:39 -0500

    Getting general flow working

--- ECE-3411-Project-2/main.c
+++ ECE-3411-Project-2/main.c
@@ -5,14 +5,16 @@
  * Author : nodcah
  */
 
+#define F_CPU 16000000UL
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <math.h>
 #include "uart.h"
+#include "bno055.h"
 
-typedef struct {
-    int x = 0;
-    int y = 0;
-    int z = 0;
-} Vector;
+// multiplying factor for bno data to radians (2*pi)/(360*16)
+#define DATA_TO_RAD 0.00109082986
 
 uint8_t readFlag = 0;  // Flag to determine when to read
 
@@ -28,19 +30,28 @@ ISR(TIMER1_COMPA_vect) {
 
 int main(void) {
     initIO();
+    initTimers();
     uart_init();
-    initBNO055();
+    //initBNO055();
     sei();
     printf("Init\n");
 
     while (1) {
         if (readFlag) {
+            printf("Reading\n");
             readFlag=0;
-            getPotentiometer();
-            BNO_Data data = getBNO055();
+            double speed = getPotentiometer();
+            BNO_Data data = {1000, 0, 0};//getBNO055();
+            printf("Pot: %.5f\n", speed);
 
             // Send data out to computer via UART
-
+            double dx = speed*sin(((double)data.x)*DATA_TO_RAD);
+            double dy = speed*cos(((double)data.x)*DATA_TO_RAD);
+            double dz = speed*sin(((double)data.y)*DATA_TO_RAD);
+            putchar(0x78);
+            putDouble(dx);
+            putDouble(dy);
+            putDouble(dz);
         }
     }
 }
@@ -48,13 +59,16 @@ int main(void) {
 void initTimers() {
     // Timer 1: CTC mode, 200 ms
     TCCR1B |= (1<<WGM12);  // Clear on Compare A
-    OCR1A = 5000;          // Set number of ticks for Compare A
+    OCR1A = 12500;          // Set number of ticks for Compare A
     TIMSK1 = (1<<OCIE1A);  // Enable Timer 1 Compare A ISR
     TCCR1B |= 4;           // Set Prescalar & Timer 1 starts
 }
 
 void initIO() {
     // init ADC
+    ADMUX |= 1<<MUX2 | 1<<MUX1;  // Use ADC 6
+    // Enable ADC and set prescalar to 128
+    ADCSRA |= 1<<ADEN | 1<<ADPS0 | 1<<ADPS1 | 1<<ADPS2;
 }
 
 void putDouble(double x) {
@@ -64,6 +78,10 @@ void putDouble(double x) {
     }
 }
 
-void getPotentiometer() {
-    //TODO implement all ADC stuff
+double getPotentiometer() {
+    // Read pot value to get speed
+    ADCSRA |= 1<<ADSC;  // Start conversion
+    while ((ADCSRA & (1<<ADSC)) != 0);  // Wait for read
+    int input = ADC;  // Read registers
+    return (((double)input)/1023.0);  // return as double between 0 and 1
 }
